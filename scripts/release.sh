ac6ef6747d1b51bad1bf087efd86dabfd7440dcd
Alina Rublea <rublea@adobe.com>
2023-06-02 15:19:13 +0300

    deploy services to kubernetes

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,8 @@
- docker image tag franklin-site-status-server:latest docker-experience-success-release.dr-uw2.adobeitc.com/franklin/site-status-server:latest
- docker image push docker-experience-success-release.dr-uw2.adobeitc.com/franklin/site-status-server:latest
+ docker image tag franklin-site-status-server:$1 docker-experience-success-release.dr-uw2.adobeitc.com/franklin/site-status-server:$1
+ docker image push docker-experience-success-release.dr-uw2.adobeitc.com/franklin/site-status-server:$1
 
- docker image tag franklin-site-status-audit-worker:latest docker-experience-success-release.dr-uw2.adobeitc.com/franklin/site-status-audit-worker:latest
- docker image push docker-experience-success-release.dr-uw2.adobeitc.com/franklin/site-status-audit-worker:latest
+ docker image tag franklin-site-status-audit-worker:$1 docker-experience-success-release.dr-uw2.adobeitc.com/franklin/site-status-audit-worker:$1
+ docker image push docker-experience-success-release.dr-uw2.adobeitc.com/franklin/site-status-audit-worker:$1
 
- docker image tag franklin-site-status-import-worker:latest docker-experience-success-release.dr-uw2.adobeitc.com/franklin/site-status-import-worker:latest
- docker image push docker-experience-success-release.dr-uw2.adobeitc.com/franklin/site-status-import-worker:latest
+ docker image tag franklin-site-status-import-worker:$1 docker-experience-success-release.dr-uw2.adobeitc.com/franklin/site-status-import-worker:$1
+ docker image push docker-experience-success-release.dr-uw2.adobeitc.com/franklin/site-status-import-worker:$1
