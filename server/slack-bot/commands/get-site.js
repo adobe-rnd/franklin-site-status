a943f848238e9015aef9f4ddfca1e03ee99a0877
Dominique Jäggi <djaeggi@adobe.com>
2023-06-08 16:17:20 +0200

    chore: optimize get-site command

--- server/slack-bot/commands/get-site.js
+++ server/slack-bot/commands/get-site.js
@@ -6,26 +6,19 @@ const { extractDomainFromInput, sendMessageBlocks, postErrorMessage } = require(
 
 const BACKTICKS = '```';
 const CHARACTER_LIMIT = 2500;
-const PADDING_EXTRA_SPACE = 2;
 const PHRASES = ['get domain', 'get site with domain'];
 
 /**
  * Formats a single row of the table, padding each cell according to the column widths.
  *
- * @param {Array} row - An array of strings, each representing a cell in the row.
- * @param {Array} columnWidths - An array of numbers, each representing the maximum width of a column.
+ * @param {Array<string>} row - An array of strings, each representing a cell in the row.
+ * @param {Array<number>} columnWidths - An array of numbers, each representing the maximum width of a column.
  * @returns {string} The formatted row.
  */
 function formatRows(row, columnWidths) {
-  return row.map((cell, i) => {
-    if (i === 0) {
-      return cell.padEnd(columnWidths[i]);
-    } else if (row.length === 2 && i === 1) {
-      return cell.padEnd(columnWidths[i] * 4);
-    } else {
-      return cell.padEnd(columnWidths[i] + PADDING_EXTRA_SPACE);
-    }
-  }).join("  ");
+  return row.map((cell, i) =>
+    cell.padEnd(columnWidths[i] + (i === 0 ? 0 : 2))
+  ).join("  ");
 }
 
 /**
@@ -33,20 +26,19 @@ function formatRows(row, columnWidths) {
  * it returns a fallback message. If the formatted table exceeds the character limit, it is sliced and appended
  * with an ellipsis.
  *
- * @param {Array} audits - An array of audit objects.
+ * @param {Array<Object>} audits - An array of audit objects.
  * @returns {string} The audits formatted into a stringified table or a fallback message.
  */
 function formatAudits(audits) {
-  if (!Array.isArray(audits)) {
+  if (!audits || !audits.length) {
     return "No audit history available";
   }
 
   const headers = ["Audited At (UTC)", "Performance", "SEO", "Accessibility", "Best Practices"];
-  const rows = audits.map((audit) => {
+  const rows = audits.map(audit => {
     const { auditedAt, errorMessage, isError } = audit;
 
     if (isError) {
-      // When isError is true, return only two columns
       return [formatDate(auditedAt), errorMessage];
     } else {
       const { performance, seo, accessibility, bestPractices } = extractAuditScores(audit);
@@ -61,36 +53,16 @@ function formatAudits(audits) {
   });
 
   const table = [headers, ...rows];
-  const columnWidths = table.reduce((widths, row) => {
-    return row.map((cell, i) => {
-      // if the row has only 2 elements (colspan case), and the cell is not the first one
-      const colSpan = row.length === 2 && i !== 0 ? 4 : 1;
-      // Ignore columns that are part of a colspanned cell
-      if (colSpan > 1 && i !== 0) {
-        return widths[i] || 0;
-      }
-      return Math.max(widths[i] || 0, cell.length / colSpan);
-    });
-  }, []);
+  const columnWidths = table[0].map((_, i) => Math.max(...table.map(row => (row[i] || '').length)));
 
+  const formattedTable = `${BACKTICKS}\n${table.map(row => formatRows(row, columnWidths)).join("\n")}\n${BACKTICKS}`;
 
-  let formattedTable = table
-    .map((row) => formatRows(row, columnWidths))
-    .join("\n");
-
-  // ensure the formattedTable string does not exceed the character limit.
-  // this is mostly due to the 3001 character limit of slack messages.
-  if (formattedTable.length > CHARACTER_LIMIT) {
-    formattedTable = formattedTable.slice(0, CHARACTER_LIMIT) + '...';
-  }
-
-  return `${BACKTICKS}\n${formattedTable}\n${BACKTICKS}`;
+  // Ensure the formattedTable string does not exceed the Slack message character limit.
+  return formattedTable.length > CHARACTER_LIMIT ? `${formattedTable.slice(0, CHARACTER_LIMIT)}...` : formattedTable;
 }
 
 /**
- * A factory function that creates an instance of the GetSiteCommand. The instance includes an `execute` method
- * that retrieves the audit status for a Franklin site by a given domain and communicates the status back via
- * the provided `say` function. If an error occurs during execution, an error message is sent back.
+ * A factory function that creates an instance of the GetSiteCommand.
  *
  * @param {Object} bot - The bot object.
  * @returns {Object} An instance of the GetSiteCommand.
@@ -99,17 +71,16 @@ function GetSiteCommand(bot) {
   const baseCommand = BaseCommand({
     id: 'get-franklin-site-status',
     name: "Get Franklin Site Status",
-    description: 'Retrieves audit status for a franklin site by a given domain',
+    description: 'Retrieves audit status for a Franklin site by a given domain',
     phrases: PHRASES,
     usageText: `${PHRASES.join(' or ')} {domain};`,
   });
 
   /**
-   * Execute function for GetSiteCommand. This function extracts the domain from the incoming message,
-   * retrieves the site status from the database using the domain, and responds back to the user with
-   * either the site status or an error message if the domain was not found.
+   * Executes the GetSiteCommand. Retrieves the audit status for a site by a given domain and communicates the status back via
+   * the provided say function. If an error occurs during execution, an error message is sent back.
    *
-   * @param {Array} args - The arguments provided to the command.
+   * @param {Array<string>} args - The arguments provided to the command.
    * @param {Function} say - The function provided by the bot to send messages.
    * @returns {Promise} A promise that resolves when the operation is complete.
    */
@@ -133,16 +104,19 @@ function GetSiteCommand(bot) {
       }
 
       const psiURL = `https://developers.google.com/speed/pagespeed/insights/?url=${site.domain}&strategy=mobile`;
+      const psiProdURL = site.isLive && site.prodDomain ? `https://developers.google.com/speed/pagespeed/insights/?url=${site.prodDomain}&strategy=mobile` : null;
 
-      let textSections = [{
+      const textSections = [{
         text: `
-      *Franklin Site Status*: ${site.domain}
-      :github-4173: GitHub: ${site.gitHubURL}
-      :lighthouse: <${psiURL}|Run PSI>
-      :clock1: Last audit on ${formatDate(site.lastAudited)}
+    *Franklin Site Status*: ${site.domain}
+    :github-4173: GitHub: ${site.gitHubURL}
+    ${site.isLive ? ':white_check_mark:' : ':x:'} Is Live: ${site.isLive ? 'Yes' : 'No'}
+    ${site.prodDomain ? `:earth_americas: Production Domain: ${site.prodDomain}` : ''}
+    :lighthouse: <${psiURL}|Run PSI (.live)> ${psiProdURL ? ` | <${psiProdURL}|Run PSI (Prod)>` : ''}
+    :clock1: Last audit on ${formatDate(site.lastAudited)}
 
-      _Audits are sorted by date descending._\n${formatAudits(site.audits)}
-      `,
+    _Audits are sorted by date descending._\n${formatAudits(site.audits)}
+  `,
       }];
 
       await sendMessageBlocks(say, textSections);
