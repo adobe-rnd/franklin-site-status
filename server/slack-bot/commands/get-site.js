f68b7a048ea61f0a8adba84393c7bff333c4b9f7
Dominique Jäggi <djaeggi@adobe.com>
2023-09-12 15:50:33 +0200

    fix: bugs

--- server/slack-bot/commands/get-site.js
+++ server/slack-bot/commands/get-site.js
@@ -17,9 +17,10 @@ const PHRASES = ['get site', 'get domain'];
  */
 function formatRows(row, columnWidths) {
   return row.map((cell, i) => {
-    // Ensure the cell is a string before calling padEnd
     const cellStr = cell || '';
-    return cellStr.padEnd(columnWidths[i] + (i === 0 ? 0 : 2));
+    // If the row has fewer columns than headers, pad the last cell to fill the remaining space
+    const padding = (row.length < headers.length && i === row.length - 1) ? columnWidths.slice(i).reduce((a, b) => a + b, 0) : columnWidths[i];
+    return cellStr.padEnd(padding + (i === 0 ? 0 : 2));
   }).join("  ");
 }
 
@@ -41,7 +42,7 @@ function formatAudits(audits) {
     const { auditedAt, errorMessage, isError } = audit;
 
     if (isError) {
-      return [formatDate(auditedAt), errorMessage];
+      return [formatDate(auditedAt), `Error: ${errorMessage}`];
     } else {
       const { performance, seo, accessibility, bestPractices } = extractAuditScores(audit);
       return [
